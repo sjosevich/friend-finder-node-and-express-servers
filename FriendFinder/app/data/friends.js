afbdb1087fdca847eb33c7d0a1dd059943c61fc6
Serguei Rodriguez <sjosevich@gmail.com>
2020-01-11 18:24:00 -0700

    Created the Json in the friends.js file

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+   {
+      name: "Ranil Joshua",
+      photo: "https://media-exp2.licdn.com/dms/image/C5103AQEKjkjw3w7tJw/profile-displayphoto-shrink_800_800/0?e=1584576000&v=beta&t=KCeZTgV7MIZkJlZYHfk6w8h783nMWDp3dzERokrfuOQ",
+      scores: [5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+    name: "Elias Luna",
+    photo: "https://media-exp2.licdn.com/dms/image/C5603AQGqr2UO8N1jyQ/profile-displayphoto-shrink_100_100/0?e=1584576000&v=beta&t=Ka7iwCHgfZlGQYQePqgnjRQmd6DITnzJtnjVrZ6BD_o",
+    scores: [5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+    name: "Oliver Pantoja",
+    photo: "https://www.linkedin.com/in/oliver-pantoja-7a5b5663/?lipi=urn%3Ali%3Apage%3Ad_flagship3_people_connections%3B8wJiVjFtQ%2FOyGJdY12sU6A%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_people_connections-connection_profile",
+    scores: [5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+    name: "Eddy Gomez",
+    photo: "https://www.linkedin.com/in/eddy-gomez-rodriguez-773b4863/?lipi=urn%3Ali%3Apage%3Ad_flagship3_people_connections%3B8wJiVjFtQ%2FOyGJdY12sU6A%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_people_connections-connection_profile",
+    scores: [5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+    name: "Mohamed Enjai",
+    photo: "https://www.linkedin.com/in/mohamed-ali-086590111/?lipi=urn%3Ali%3Apage%3Ad_flagship3_people_connections%3B8wJiVjFtQ%2FOyGJdY12sU6A%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_people_connections-connection_profile",
+    scores: [5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+    name: "Alexis Rodriguez",
+    photo: "https://www.linkedin.com/in/alexis-rodriguez-rodriguez-4a0904161/?lipi=urn%3Ali%3Apage%3Ad_flagship3_people_connections%3B8wJiVjFtQ%2FOyGJdY12sU6A%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_people_connections-connection_profile",
+    scores: [5,1,4,4,5,1,2,5,4,1]
+   }
+
+
+];
+
+module.exports = friends
--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -19,7 +19,14 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
